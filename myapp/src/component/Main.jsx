0ac77de164962a5edd9f4e94fd2ca98357f0fc8b
M Bilal <95403773+SajadiaHouse@users.noreply.github.com>
2023-11-05 11:34:03 +0500

    commit

--- myapp/src/component/Main.jsx
+++ myapp/src/component/Main.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Notification from './Notification'
-
+import data from './data.json'
 const Main = () => {
+       
+
     return (
         <>
             <div className="flex justify-center items-center my-8">
-                <div className="md:w-[50%] bg-white md:shadow-slate-300 p-6 rounded">
-                    <p className='font-semibold float-left text-lg'>Notification</p>
+                <div className="md:w-[55%] bg-white md:shadow-slate-300 p-6 rounded">
+                    <p className='font-semibold float-left text-2xl'>Notification <span className='w-3 h-3 bg-blue-900 text-white rounded px-3 py-1 ml-2 text-sm font-normal'>3</span></p>
                     <p><a className='float-right cursor-pointer' href="">Mark all read</a></p>
                     <div className='flex-col mt-8 p-6'>
-<Notification />
-<Notification />
+                        {data.map((e)=>(
+                            <Notification img={e.img} name={e.name} react={e.react} post={e.post} time={e.time} msg={e.msg} indicator={e.indicator} like={e.like} />
+                        ))}
                     </div>
-                    
                 </div>
             </div>
         </>
--- myapp/src/component/Notification.jsx
+++ myapp/src/component/Notification.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
+import React from 'react';
 
-const Notification = () => {
+const Notification = ({img,name, post,react,time,msg,indicator,like}) => {
     return (
         <>
-            <div className='hover:bg-blue-100 rounded p-3 mt-2'>
-                <img src="./images/avatar-angela-gray.webp" alt="" className='mr-4 block w-12 h-12' />
-                <div className='text-left w-full'>
- <span className='font-semibold cursor-pointer'>Mark Webber</span>
-                    <span>reacted to your recent post</span>
-                    <span className='font-semibold cursor-pointer'>My Frist tumonent today!</span>
-                  <span className='bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300'>.</span>
+        <div className="hover:bg-blue-100 rounded p-3 mt-2 flex items-start">
+            <img
+                src={`./images/${img}`}
+                alt=""
+                className="mr-4 block w-12 h-12"
+            />
+            <div className="text-left w-full">
+                <div className="flex items-center">
+                    <div className='space-x-2'>
+                    <span className="font-semibold cursor-pointer hover:text-blue-900">
+                        {name}
+                    </span>
+                    <span className='text-slate-500'>{react}</span>
+                    <span className="font-semibold cursor-pointer  hover:text-blue-900">
+                       {post}
+                    </span>
+                    </div>
+                    {indicator==="yes" ? <span class="w-2 h-2 ml-2 bg-red-500 rounded-full self-center"></span>: ""}
+                    {like ? <img src={like} className="w-12 h-12 ml-auto" /> :"" }
+                    
                 </div>
-                   
-                
-                    <span className='font-thin text-slate-400'>1m ago</span>
+                <p className="text-slate-400 text-sm">{time}</p>
+               
             </div>
+            
+        </div>
+        {msg ? <div className='hover:bg-blue-100 text-base p-3 rounded -mt-3 text-slate-800 ml-16 mr-10'>{msg}</div>:"" }
 
-        </>
-    )
-}
+</>
+    );
+};
 
-export default Notification
+export default Notification;
